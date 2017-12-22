10c97155646a81cf79bf8b89783686c1593b81a2
Frederic Boudon <fredboudon@users.noreply.github.com>
2017-12-22 10:24:57 +0100

    Change version of conda-build
    Still broken. Test another version of conda-build

--- install.bat
+++ install.bat
@@ -48,7 +48,7 @@ if errorlevel 1 exit 1
 
 conda update conda
 if errorlevel 1 exit 1
-conda install conda-build=3.1.6 anaconda-client
+conda install conda-build=3.0.31 anaconda-client
 if errorlevel 1 exit 1
 
 for /f %%i in ('python python_version.py') DO (set PYTHON_VERSION=%%i)
